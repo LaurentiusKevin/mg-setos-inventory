0bd1a2ac6d39750741053572194f84613a43a0e6
kevin <OPUSdei@21>
2021-05-08 09:03:50 +0700

    Ganti nama aplikasi

--- resources/views/components/admin/footer-component.blade.php
+++ resources/views/components/admin/footer-component.blade.php
@@ -1,4 +1,4 @@
 <footer class="c-footer">
-    <div><a href="{{ url('') }}">MG Setos Inventory</a> © {{ date('Y') }} NerualSys.</div>
+    <div><a href="{{ url('') }}">Setos Purchasing Program</a> © {{ date('Y') }} NerualSys.</div>
 {{--            <div class="ml-auto">Powered by&nbsp;<a href="https://coreui.io/">CoreUI</a></div>--}}
 </footer>
